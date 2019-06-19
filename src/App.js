fada860952d7ae5b8b04244dabd9326a51478ade
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-06-19 19:18:09 +0100

    The user latest 5 public repos are displayed

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -62,6 +63,20 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  //Get users repos
+
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=createdäsc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   //Set Alert
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
@@ -70,7 +85,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
     return (
       <Router>
         <div className="App">
@@ -101,7 +116,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
