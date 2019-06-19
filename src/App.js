e3c71582bbd13472e1871d114021b941c5df87a5
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-06-19 16:36:00 +0100

    User can now search for GH users, added props to the Search component

--- src/App.js
+++ src/App.js
@@ -10,21 +10,41 @@ class App extends Component {
     users: [],
     loading: false
   };
+
   async componentDidMount() {
+    //console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
     this.setState({ loading: true });
 
-    const res = await axios.get("https://api.github.com/users");
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
     this.setState({ users: res.data, loading: false });
 
-    console.log(res.data);
+    //console.log(res.data);
   }
+
+  //Search GitHub users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    console.log(text);
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
